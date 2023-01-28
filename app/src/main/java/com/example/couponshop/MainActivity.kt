76a7e97eace751537c7050460bd0318a6067e4c7
Melanie Mantilla <lany033@hotmail.com>
2023-01-28 17:16:26 -0500

    archivos_base_activity_main_&_xml

--- app/src/main/java/com/example/couponshop/MainActivity.kt
+++ app/src/main/java/com/example/couponshop/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.couponshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val rvCoupons: RecyclerView = findViewById(R.id.rvCoupons)
+        //rvCoupons.layoutManager = LinearLayoutManager(this)
+        //val coupons = ArrayList<Coupon>()
+
     }
 }
